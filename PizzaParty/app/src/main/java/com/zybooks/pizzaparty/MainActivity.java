cb48a3db51b0b767ad6dff7ca5c64432497e1787
Kieran Sukachevin <kieran@Kierans-MacBook-Pro-2.local>
2021-04-08 17:24:38 -0700

    Spinner added, radio buttons removed, formating changed for text edit box

--- PizzaParty/app/src/main/java/com/zybooks/pizzaparty/MainActivity.java
+++ PizzaParty/app/src/main/java/com/zybooks/pizzaparty/MainActivity.java
@@ -1,12 +1,18 @@
 package com.zybooks.pizzaparty;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText mNumAttendEditText;
     private TextView mNumPizzasTextView;
-    private RadioGroup mHowHungryRadioGroup;
+    private Spinner mHowHungrySpinner;
     private final static String TAG = "MainActivity";
 
     @Override
@@ -25,7 +31,41 @@ public class MainActivity extends AppCompatActivity {
         // Assign the widgets to fields
         mNumAttendEditText = findViewById(R.id.attendEditText);
         mNumPizzasTextView = findViewById(R.id.answerTextView);
-        mHowHungryRadioGroup = findViewById(R.id.hungryRadioGroup);
+        mHowHungrySpinner = findViewById(R.id.hungrySpinner);
+
+        mNumAttendEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                mNumPizzasTextView.setText("");
+            }
+        });
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.options_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mHowHungrySpinner.setAdapter(adapter);
+        mHowHungrySpinner.setSelection(0, false);
+        mHowHungrySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = (String)parent.getItemAtPosition(position);
+                Toast.makeText(MainActivity.this, item, Toast.LENGTH_SHORT).show();
+                mNumPizzasTextView.setText("");
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
 
         Log.d(TAG, "onCreate was called");
     }
@@ -43,21 +83,22 @@ public class MainActivity extends AppCompatActivity {
         }
 
         // Get hunger level selection
-        int checkedId = mHowHungryRadioGroup.getCheckedRadioButtonId();
-        PizzaCalculator.HungerLevel hungerLevel = PizzaCalculator.HungerLevel.RAVENOUS;
-        if (checkedId == R.id.lightRadioButton) {
-            hungerLevel = PizzaCalculator.HungerLevel.LIGHT;
-        }
-        else if (checkedId == R.id.mediumRadioButton) {
-            hungerLevel = PizzaCalculator.HungerLevel.MEDIUM;
-        }
+//        int checkedId = mHowHungrySpinner.get();
+//        PizzaCalculator.HungerLevel hungerLevel = PizzaCalculator.HungerLevel.RAVENOUS;
+//        if (checkedId == R.id.lightRadioButton) {
+//            hungerLevel = PizzaCalculator.HungerLevel.LIGHT;
+//        }
+//        else if (checkedId == R.id.mediumRadioButton) {
+//            hungerLevel = PizzaCalculator.HungerLevel.MEDIUM;
+//        }
 
-        // Get the number of pizzas needed
-        PizzaCalculator calc = new PizzaCalculator(numAttend, hungerLevel);
-        int totalPizzas = calc.getTotalPizzas();
-
-        // Place totalPizzas into the string resource and display
-        String totalText = getString(R.string.total_pizzas, totalPizzas);
-        mNumPizzasTextView.setText(totalText);
+//        // Get the number of pizzas needed
+//        PizzaCalculator calc = new PizzaCalculator(numAttend, hungerLevel);
+//        int totalPizzas = calc.getTotalPizzas();
+//
+//        // Place totalPizzas into the string resource and display
+//        String totalText = getString(R.string.total_pizzas, totalPizzas);
+//        mNumPizzasTextView.setText(totalText);
     }
+
 }
