c57b6c0b824eb2d2a0d57c2a502f6d86661ecdf9
Kieran Sukachevin <kieran@Kierans-MacBook-Pro-2.local>
2021-04-11 16:33:28 -0700

    Widgets added and styles changed

--- Drawit/app/src/main/java/edu/wallawalla/cs/sukaki/drawit/MainActivity.java
+++ Drawit/app/src/main/java/edu/wallawalla/cs/sukaki/drawit/MainActivity.java
@@ -3,12 +3,80 @@ package edu.wallawalla.cs.sukaki.drawit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner mOptionsTab;
+    private Spinner mColorTab;
+    private SeekBar mSizeBar;
+    private int penSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mOptionsTab = findViewById(R.id.optionsTab);
+        mColorTab = findViewById(R.id.colorTab);
+        mSizeBar = findViewById(R.id.seekBar);
+
+        ArrayAdapter<CharSequence> adapterOptions = ArrayAdapter.createFromResource(this,
+                R.array.options_array, android.R.layout.simple_spinner_item);
+        adapterOptions.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mOptionsTab.setAdapter(adapterOptions);
+        mOptionsTab.setSelection(0, false);
+        mOptionsTab.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = (String)parent.getItemAtPosition(position);
+                Toast.makeText(MainActivity.this, item, Toast.LENGTH_SHORT).show();
+//                mNumPizzasTextView.setText("");
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        ArrayAdapter<CharSequence> adapterColor = ArrayAdapter.createFromResource(this,
+                R.array.color_array, android.R.layout.simple_spinner_item);
+        adapterColor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mColorTab.setAdapter(adapterColor);
+        mColorTab.setSelection(0, false);
+        mColorTab.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = (String)parent.getItemAtPosition(position);
+                Toast.makeText(MainActivity.this, item, Toast.LENGTH_SHORT).show();
+//                mNumPizzasTextView.setText("");
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        mSizeBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                // progress ranges from 0 to max
+                penSize = progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
     }
 }
